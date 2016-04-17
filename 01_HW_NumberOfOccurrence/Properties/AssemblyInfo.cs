3d1916b26ff02a290a18ecf4807ae59603803835
Beytula Redzheb <beytularedzheb@gmail.com>
2016-04-17 17:47:18 +0300

    Project titles and location fix

--- 01_HW_NumberOfOccurrence/Properties/AssemblyInfo.cs
+++ 01_HW_NumberOfOccurrence/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSC_Preparation")]
+[assembly: AssemblyTitle("01_HW_NumberOfOccurrence")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSC_Preparation")]
+[assembly: AssemblyProduct("01_HW_NumberOfOccurrence")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- 04_HW_HashTableImplementation/Properties/AssemblyInfo.cs
+++ 04_HW_HashTableImplementation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("01_HW_HashTableImplementation")]
+[assembly: AssemblyTitle("04_HW_HashTableImplementation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("01_HW_HashTableImplementation")]
+[assembly: AssemblyProduct("04_HW_HashTableImplementation")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
