3263df3943f32a9d95fec4da730f4395c2354cd9
Beytula Redzheb <beytularedzheb@gmail.com>
2016-04-18 23:11:17 +0300

    '06_Sorting' is changed to '06_SomeAlgorithms'

--- 06_SomeAlgorithms/Properties/AssemblyInfo.cs
+++ 06_SomeAlgorithms/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("06_Sorting")]
+[assembly: AssemblyTitle("06_SomeAlgorithms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("06_Sorting")]
+[assembly: AssemblyProduct("06_SomeAlgorithms")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
